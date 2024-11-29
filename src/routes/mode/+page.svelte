676baf831c0126c59f8d29843c8e3cf958587337
Anna Maier <anna.maier@codecentric.de>
2024-11-29 11:11:12 +0100

    Re-organise and update light/dark mode examples

--- src/routes/mode/+page.svelte
+++ src/routes/mode/+page.svelte
@@ -1,24 +1,23 @@
 <script>
-	const example1 = `.example1 {<br>
+	const styledWithSystemColors = `.styled-with-system-colors {<br>
 &nbsp;&nbsp;color-scheme: light dark;<br>
 &nbsp;&nbsp;color: CanvasText;<br>
 &nbsp;&nbsp;background-color: Canvas;<br>
 }`;
-	const example2 = `.example2 {<br>
+	const styledWithLightDark = `.styled-with-light-dark {<br>
 &nbsp;&nbsp;color-scheme: light dark;<br>
 &nbsp;&nbsp;color: light-dark(#731, #dcb);<br>
 &nbsp;&nbsp;background-color: light-dark(#dca, #753);<br>
 }`;
 
-	const example3 = `.example3 {<br>
+	const mediaQuery = `.styled-with-media-query {<br>
 &nbsp;&nbsp;background: #dca;<br>
 &nbsp;&nbsp;color: #731;<br>
 }<br>
 @media (prefers-color-scheme: dark) {<br>
-&nbsp;&nbsp;.example3 {<br>
+&nbsp;&nbsp;.styled-with-media-query {<br>
 &nbsp;&nbsp;&nbsp;background: #753;<br>
 &nbsp;&nbsp;&nbsp;color: #dcb;<br>
-&nbsp;&nbsp;&nbsp;outline: 5px dashed #000;<br>
 &nbsp;&nbsp;}<br>
 }`;
 
@@ -37,22 +36,35 @@
 
 	<p>Toggle light and dark mode in OS to see the changes on this page.</p>
 
-	<div class="example1 flex w-[750px] flex-col gap-4 p-4">
-		<h2>Using the color-scheme CSS property</h2>
-		<samp>{@html example1}</samp>
+	<div class="flex flex-col gap-4 py-4">
+		<h2>Checking mode with prefers-color-scheme media query</h2>
+		<samp>{@html mediaQuery}</samp>
+		<div class="styled-with-media-query">This is the styled box</div>
+		<p>
+			Link to documentation:
+			<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme">
+				prefers-color-scheme</a>
+		</p>
+	</div>
+
+	<div class="flex flex-col gap-4 py-4">
+		<h2>Using system colors for dark/light mode</h2>
+		<samp>{@html styledWithSystemColors}</samp>
+		<div class="styled-with-system-colors">This is the styled box</div>
 		<p>
 			Link to documentation:
 			<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color-scheme">color-scheme</a>
 		</p>
 	</div>
 
-	<div class="example2 flex w-[750px] flex-col gap-4 p-4">
+	<div class="flex flex-col gap-4 py-4">
 		<h2>Using light-dark CSS property</h2>
-		<samp>{@html example2}</samp>
+		<samp>{@html styledWithLightDark}</samp>
 		<p>
 			Not (yet) supported in all browsers, check
 			<a href="https://caniuse.com/?search=light-dark">can-i-use page</a>.
 		</p>
+		<div class="styled-with-light-dark">This is the styled box</div>
 		<p>
 			Link to documentation:
 			<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/light-dark">
@@ -61,17 +73,7 @@
 		</p>
 	</div>
 
-	<div class="example3 flex w-[750px] flex-col gap-4 p-4">
-		<h2>Using prefers-color-scheme media query</h2>
-		<samp>{@html example3}</samp>
-		<p>
-			Link to documentation:
-			<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme">
-				prefers-color-scheme</a>
-		</p>
-	</div>
-
-	<div class="example1 flex w-[750px] flex-col gap-4 p-4">
+	<div class="styled-with-system-colors flex flex-col gap-4 py-4">
 		<h2 class="text-xl">Focus outlines</h2>
 		<p>
 			Focus outlines should be visible regardless of light or dark mode. This can be done with an
@@ -89,43 +91,35 @@
 </div>
 
 <style>
-	.example1 {
+	.styled-with-media-query {
+		background: #cedce8;
+		color: #3d4245;
+		padding: 1rem 1rem;
+	}
+	@media (prefers-color-scheme: dark) {
+		.styled-with-media-query {
+			background: #3d4245;
+			color: #cedce8;
+		}
+	}
+	.styled-with-system-colors {
 		color-scheme: light dark;
 		color: CanvasText;
 		background-color: Canvas;
+		border: 1px solid ButtonBorder;
+		padding: 1rem 1rem;
 	}
 
-	.example2 {
+	.styled-with-light-dark {
 		color-scheme: light dark;
-		color: light-dark(#731, #dcb);
-		background-color: light-dark(#dca, #753);
-	}
-
-	.example3 {
-		background: #dca;
-		color: #731;
-	}
-	@media (prefers-color-scheme: dark) {
-		.example3 {
-			background: #753;
-			color: #dcb;
-		}
+		color: light-dark(#3d4245, #cedce8);
+		background-color: light-dark(#cedce8, #3d4245);
+		padding: 1rem 1rem;
 	}
 
 	button:focus-visible {
 		outline: 3px solid black;
 		box-shadow: 0 0 0 6px white;
-	}
-
-	a {
-		color-scheme: light dark;
-		color: CanvasText;
-		text-decoration: underline;
-	}
-
-	a:hover {
-		color-scheme: light dark;
-		color: light-dark(var(--color-theme-3), var(--color-theme-4));
 	}
 
 	button:hover {
