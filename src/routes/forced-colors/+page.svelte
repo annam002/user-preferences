9922511df29defa7fa9e927a8b6173c91d80d214
Anna Maier <anna.maier@codecentric.de>
2025-01-27 12:01:55 +0100

    Add example for losing visual semantics in forced colors mode

--- src/routes/forced-colors/+page.svelte
+++ src/routes/forced-colors/+page.svelte
@@ -14,7 +14,7 @@
 		<li>For simulation, you can toggle forced-colors in the rendering pane in Chrome.</li>
 	</ul>
 	<p>Forced-colors overwrites it all, but you need to pay attention to some gotchas.</p>
-	<ul class="flex flex-col gap-2">
+	<ul class="flex flex-col gap-4">
 		<li class="item-with-shadow">
 			Shadows are gone. So if you use a shadow as differentiator you need to replace it.
 		</li>
@@ -39,8 +39,19 @@
 				</div>
 			</div>
 		</li>
+		<li>
+			Only native semantics are supported. Aria-attributes are not taken into account.
+			<br />
+			One example: buttons that are disabled using only aria-disabled are not shown as disabled in forced-colors
+			mode.
+			<div class="flex gap-2">
+				<button class="wannabe-button" onclick={() => alert('Active Button Clicked')}
+					>Active button</button>
+				<button disabled class="wannabe-button">Disabled button</button>
+				<button class="wannabe-button" aria-disabled="true">Aria-disabled button</button>
+			</div>
+		</li>
 	</ul>
-	<p></p>
 	<p>
 		Link to documentation: <a
 			href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/forced-colors">forced-colors</a>
@@ -49,12 +60,14 @@
 
 <style>
 	.item-with-shadow {
+		padding: 0.5rem;
 		box-shadow:
 			-2px -2px 5px green,
 			2px 2px 5px green;
 	}
 
 	.item-with-background {
+		padding: 0.5rem;
 		background-color: #ddccaa;
 	}
 
@@ -72,5 +85,39 @@
 		.item-with-background {
 			border: 1px solid ButtonBorder;
 		}
+	}
+
+	.wannabe-button {
+		background-image: linear-gradient(#f7f8fa, #e7e9ec);
+		border-color: #adb1b8 #a2a6ac #8d9096;
+		border-style: solid;
+		border-width: 1px;
+		border-radius: 3px;
+		box-shadow: rgba(255, 255, 255, 0.6) 0 1px 0 inset;
+		box-sizing: border-box;
+		color: #0f1111;
+		cursor: pointer;
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		outline: 0;
+		padding: 0.8rem;
+	}
+
+	.wannabe-button:disabled {
+		color: #a2a6ac;
+	}
+
+	[aria-disabled='true'] {
+		color: #a2a6ac;
+	}
+
+	.wannabe-button:hover {
+		border-color: #a2a6ac #979aa1 #82858a;
+	}
+
+	.wannabe-button:focus-visible {
+		outline: 2px solid ButtonBorder;
+		outline-offset: 2px;
 	}
 </style>
